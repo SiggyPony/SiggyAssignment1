3dfd56ecdab1d2f74869e3c905be819c07fb46fe
SiggyZee <comteckbird@gmail.com>
2016-03-08 02:10:25 +1300

    More basic structure and save functionality

--- Data.py
+++ Data.py
@@ -1,5 +1,46 @@
+import pickle
+
 class Data:
+
+    # The current table of data.
+    dataSetName = ""
+    data = {}
 
     def __init__(self):
         pass
+
+    def importData(self):
+        pass
+
+    def loadData(self, loadString):
+        try:
+            inputFile = open(loadString, 'rb')
+            self.data = pickle.load(inputFile)
+            print('Loaded data from ' + loadString)
+        except Exception as err:
+            # TODO continue here
+            pass
+
+    def saveData(self, saveString):
+        try:
+            if (saveString == ''):
+                saveString = 'mydata'
+            outputFile = open(saveString, 'wb')
+            pickle.dump(self.data, outputFile)
+            print('Data set s% saved to %s' % (self.dataSetName, saveString))
+            outputFile.close()
+        except Exception as err:
+            print(err)
+            print('You give a valid file name and path to' +
+                  ' save too relative to the current directory.')
+            print('Eg. ./save/myData.dat')
+
+    def verifyLineData(self):
+        pass
+
+    def editLineData(self):
+        pass
+
+
+
 
--- Main.py
+++ Main.py
@@ -1,10 +1,34 @@
 import cmd
+
 from Data import Data
 
-class Main(cmd.Cmd):
+class Controller(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
-        self.intro = 'Welcome to Siggy\'s Data Presentation Program SDPP \n'
+        self.intro = 'Welcome to Siggy\'s Data Presentation Program - SDPP \n'
         self.prompt = 'Enter your command: \n'
         self.data = Data()
-        
+
+    def do_load_data(self, line):
+        print('load_data')
+
+    def do_save_project(self, line):
+        print('save_project')
+
+    def do_load_project(self, line):
+        print('load_project')
+
+    def do_show(self, line):
+        print('show')
+
+    def do_exit(self, line):
+        return True
+
+
+def main():
+    print('Starting')
+    controllerTemp = Controller()
+    controllerTemp.cmdloop()
+
+if __name__ == '__main__':
+    main()
